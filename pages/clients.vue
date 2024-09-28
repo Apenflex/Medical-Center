649d90ba9c9b9c16c14c2d1e682049fdb728713e
Svyatoslav <blackdbf@gmail.com>
2024-09-28 14:00:04 +0300

    Refactor sticky positioning in HeaderBlock and LayoutSidebar components

--- pages/clients.vue
+++ pages/clients.vue
@@ -3,7 +3,112 @@
 </script>
 
 <template>
-    <div>
-        <h1>Scanner Page</h1>
-    </div>
+    <section>
+        <h1>Clients Page</h1>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit cumque maiores, iusto, provident debitis numquam repudiandae minus expedita cum nostrum aliquid maxime. Assumenda ea perspiciatis id sint ratione, nam ducimus.
+        </div>
+    </section>
 </template>
